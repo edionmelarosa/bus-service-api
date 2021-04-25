105cb442aacff6d9064261c92787fa89997cc3cd
Espiridion Larosa <edion.larosa@gmail.com>
2021-04-25 10:29:36 +0800

    refactor mockdata service

--- src/mock-data/mock-data.service.ts
+++ src/mock-data/mock-data.service.ts
@@ -11,8 +11,6 @@ export class MockDataService {
     const busStops = [
       {
         name: 'Bus Stop 1', 
-        lat: '1.369256', 
-        long: '103.828619', 
         location: {
           type: 'Point',
           coordinates: [103.83972077425295, 1.335083707317516] // 216.73
@@ -24,8 +22,6 @@ export class MockDataService {
       },
       {
         name: 'Bus Stop 2', 
-        lat: '1.358702', 
-        long: '103.829048', 
         location: {
           type: 'Point',
           coordinates: [103.83770217768149, 1.3355637716640834] // 112.56
@@ -37,8 +33,6 @@ export class MockDataService {
       },
       {
         name: 'Bus Stop 3', 
-        lat: '1.350636', 
-        long: '103.836601', 
         location: {
           type: 'Point',
           coordinates: [103.8367773581069, 1.3340969080885703] // 126.54
@@ -50,8 +44,6 @@ export class MockDataService {
       },
       {
         name: 'Bus Stop 4', 
-        lat: '1.350636', 
-        long: '103.836601', 
         location: {
           type: 'Point',
           coordinates: [103.83611931340961, 1.3340613477486005] // 202.12
@@ -64,7 +56,10 @@ export class MockDataService {
     ]
 
     const busStopRepository = getMongoRepository(BusStop);
-    await busStopRepository.insertMany(busStops)
+    const count = await busStopRepository.count();
+    if (count === 0) {
+      await busStopRepository.insertMany(busStops)
+    }
     return await busStopRepository.find({});
   }
 }
